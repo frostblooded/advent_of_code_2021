e486ce5a9dce7a310eacf3d5b786b399e714189d
frostblooded <frostblooded@yahoo.com>
2021-12-18 18:50:24 +0200

    Ignore some files

--- day_1/target/debug/deps/day_1-cef8516f267c84f9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\frost\source\repos\advent_of_code_2021\day_1\target\debug\deps\day_1-cef8516f267c84f9.exe: src\main.rs
-
-C:\Users\frost\source\repos\advent_of_code_2021\day_1\target\debug\deps\day_1-cef8516f267c84f9.d: src\main.rs
-
-src\main.rs:
--- day_1/target/release/day_1.d
+++
@@ -1,1 +0,0 @@
-c:\Users\frost\source\repos\advent_of_code_2021\day_1\target\release\day_1.exe: c:\Users\frost\source\repos\advent_of_code_2021\day_1\src\main.rs
