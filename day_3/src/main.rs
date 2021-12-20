983e9945ef0c2a7a7da108150ebd10c44a890e12
frostblooded <frostblooded@yahoo.com>
2021-12-20 19:42:04 +0200

    Complete day 3

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,38 +1,57 @@
 use std::fs;
 
+fn calculate(lines: Vec<&str>, position: u8, search_for_most_common: bool) -> &str {
+    if lines.len() == 1 {
+        return lines.iter().next().unwrap();
+    }
+
+    let mut zeroes: u32 = 0;
+    let mut ones: u32 = 0;
+    let mut zero_lines: Vec<&str> = vec![];
+    let mut one_lines: Vec<&str> = vec![];
+
+    for line in lines {
+        match line.chars().nth(position.into()).unwrap() {
+            '0' => {
+                zeroes += 1;
+                zero_lines.push(line);
+            }
+            '1' => {
+                ones += 1;
+                one_lines.push(line);
+            }
+            _ => panic!("Invalid input bit"),
+        }
+    }
+
+    let comparator = if search_for_most_common {
+        |a: u32, b: u32| a > b
+    } else {
+        |a: u32, b: u32| a <= b
+    };
+
+    let resulting_lines = if comparator(zeroes, ones) {
+        zero_lines
+    } else {
+        one_lines
+    };
+
+    calculate(resulting_lines, position + 1, search_for_most_common)
+}
+
 fn main() {
     let input: String = fs::read_to_string("input.txt").expect("Couldn't read input file");
-    let mut bits_count: [(u32, u32); 12] = [(0, 0); 12];
+    let lines: Vec<&str> = input.lines().collect();
 
-    for line in input.lines() {
-        for (i, b) in line.chars().enumerate() {
-            match b {
-                '0' => bits_count[i].0 += 1,
-                '1' => bits_count[i].1 += 1,
-                _ => panic!("Invalid bit in input"),
-            };
-        }
-    }
+    let ogr = calculate(lines.clone(), 0, true);
+    let csr = calculate(lines, 0, false);
 
-    let mut gamma: String = "".to_string();
-    let mut epsilon: String = "".to_string();
+    let ogr_num: isize = isize::from_str_radix(&ogr, 2).expect("Failed to parse ogr as decimal");
+    let csr_num: isize = isize::from_str_radix(&csr, 2).expect("Failed to parse csr as decimal");
 
-    for counts in bits_count {
-        if counts.0 > counts.1 {
-            gamma.push('0');
-            epsilon.push('1');
-        } else {
-            gamma.push('1');
-            epsilon.push('0');
-        }
-    }
-
-    let gamma_num: isize =
-        isize::from_str_radix(&gamma, 2).expect("Failed to parse gamma as decimal");
-    let epsilon_num: isize =
-        isize::from_str_radix(&epsilon, 2).expect("Failed to parse epsilon as decimal");
-
-    println!("Gamma: {}", gamma_num);
-    println!("Epsilon: {}", epsilon_num);
-    println!("Multiplied: {}", gamma_num * epsilon_num);
+    println!("OGR binary: {}", ogr);
+    println!("OGR: {}", ogr_num);
+    println!("CSR binary: {}", csr);
+    println!("CSR: {}", csr_num);
+    println!("Multiplied: {}", ogr_num * csr_num);
 }
