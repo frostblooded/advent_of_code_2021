e1c5f3c5abd75dcc82ceb578ec1a4f9d2dcac7a7
frostblooded <frostblooded@yahoo.com>
2022-01-03 10:49:23 +0200

    Complete day 6

---
+++ day_6/src/main.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+const MAX_AGE: u128 = 8;
+const DAYS: u128 = 256;
+
+fn main() {
+    let input: String = fs::read_to_string("input.txt").expect("Couldn't read input file");
+    let input_ages: Vec<u8> = input
+        .split(',')
+        .map(|n| {
+            n.chars()
+                .next()
+                .unwrap()
+                .to_string()
+                .parse::<u8>()
+                .expect("Couldn't parse input number")
+        })
+        .collect();
+
+    let mut ages: Vec<u128> = vec![0; MAX_AGE as usize + 1];
+
+    for age in input_ages {
+        ages[age as usize] += 1;
+    }
+
+    dbg!(&ages);
+
+    for i in 1..=DAYS {
+        let mut new_ages = vec![0; MAX_AGE as usize + 1];
+
+        for j in 0..=(MAX_AGE - 1) {
+            new_ages[j as usize] = ages[j as usize + 1];
+        }
+
+        new_ages[MAX_AGE as usize] = ages[0];
+        new_ages[6] += ages[0];
+
+        ages = new_ages;
+        dbg!(i);
+        dbg!(&ages);
+    }
+
+    let mut res: u128 = 0;
+
+    for age in ages {
+        res += age;
+    }
+
+    println!("Result: {:?}", res);
+}
